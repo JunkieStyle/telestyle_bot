49d6494c2a09f4f8abaa98c4fffd3aa3a5c0c976
Anton Nekhaev <antonnekhaev@gmail.com>
2021-03-26 23:43:52 +0300

    added deploy to heroku

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from telegram.ext import Updater, CommandHandler
-import requests
+import os
 import logging
+import requests
+
 from urllib.parse import urljoin
+from telegram.ext import Updater, CommandHandler
+
+PORT = int(os.environ.get('PORT', 5000))
+TOKEN = os.environ.get('TOKEN')
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -11,29 +16,32 @@ logger = logging.getLogger()
 
 
 def get_meme_url(subreddit=None):
-    url = "https://meme-api.herokuapp.com/gimme"
+    url = 'https://meme-api.herokuapp.com/gimme'
     if subreddit:
-        url = urljoin(url + "/", subreddit)
+        url = urljoin(url + '/', subreddit)
 
-    logger.info("Url %s" % url)
+    logger.info('Url %s' % url)
     r = requests.get(url).json()
-    logger.info("Got json response: %s" % r)
-    return r["url"]
+    logger.info('Got json response: %s' % r)
+    return r['url']
 
 
 def send_meme(update, context):
-    logger.info("Got update: %s" % update)
-    logger.info("Got args: %s" % context.args)
+    logger.info('Got update: %s' % update)
+    logger.info('Got args: %s' % context.args)
     url = get_meme_url(*context.args)
     chat_id = update.message.chat_id
     context.bot.send_photo(chat_id=chat_id, photo=url)
 
 
 def main():
-    with open("token.txt") as f:
-        token = f.read()
-
-    updater = Updater(token, use_context=True)
+    updater = Updater(TOKEN, use_context=True)
+    updater.start_webhook(
+        listen='0.0.0.0',
+        port=int(PORT),
+        url_path=TOKEN
+    )
+    updater.bot.setWebhook('https://telestyle-bot.herokuapp.com/' + TOKEN)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('meme', send_meme, pass_args=True))
     updater.start_polling()
