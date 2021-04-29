0c473dd9f2198dd976771f9781f60b92dfd62e98
Anton Nekhaev <antonnekhaev@gmail.com>
2021-04-29 23:13:15 +0300

    added radio command

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import requests
+import vk_api
 
 from urllib.parse import urljoin
 
@@ -10,6 +11,9 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 PORT = int(os.environ.get("PORT", 5000))
 TOKEN = os.environ["TOKEN"]
 NAME = os.environ["NAME"]
+VK_USERNAME = os.environ["VK_USERNAME"]
+VK_PASSWORD = os.environ["VK_PASSWORD"]
+
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -28,6 +32,20 @@ def get_meme_url(subreddit: str = None):
     return r["url"]
 
 
+def get_radio_url():
+    vk_session = vk_api.VkApi(VK_USERNAME, VK_PASSWORD)
+    vk_session.auth()
+
+    vk = vk_session.get_api()
+
+    owner_id = -29534144  # lentach
+    data = vk.wall.search(owner_id=owner_id, count=1, query="#радиолентач")
+    logger.info("Got data: %s" % data)
+    post_id = data["items"][0]["id"]
+
+    return f"https://vk.com/wall{owner_id}_{post_id}"
+
+
 def error_handler(update: Update, context: CallbackContext):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
     chat_id = update.message.chat_id
@@ -42,12 +60,20 @@ def send_meme(update: Update, context: CallbackContext):
     context.bot.send_photo(chat_id=chat_id, photo=url)
 
 
+def send_radio(update: Update, context: CallbackContext):
+    logger.info("Got update: %s" % update)
+    url = get_radio_url()
+    chat_id = update.message.chat_id
+    context.bot.send_message(chat_id=chat_id, photo=url)
+
+
 def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
     # noinspection PyTypeChecker
     dp.add_error_handler(error_handler)
     dp.add_handler(CommandHandler("meme", send_meme, pass_args=True))
+    dp.add_handler(CommandHandler("radio", send_radio))
 
     logging.info(f"Start webhook mode on port {PORT}")
     updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
